2ee252373a5167886c34ba470af5ce878425cc8e
BNivia <nobenjamin19@gmail.com>
2021-05-20 01:00:51 -0500

    Added decrement.

--- src/App.js
+++ src/App.js
@@ -35,6 +35,14 @@ class App extends Component{
         this.setState({counters});
     }
 
+    handleDecrement = (counter) => {
+      const counters = [...this.state.counters];
+      const index = counters.indexOf(counter);
+      counters[index] = {...counter};
+      counters[index].value--;
+      this.setState({counters});
+    }
+
   render(){
     return (
       <React.Fragment>
@@ -44,6 +52,7 @@ class App extends Component{
             counters={this.state.counters}
             onReset={this.handleReset} 
             onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
             onDelete={this.handleDelete}
             />
         </main>
@@ -54,3 +63,6 @@ class App extends Component{
 }
 
 export default App;
+
+
+//didMount is good for ajax calls
